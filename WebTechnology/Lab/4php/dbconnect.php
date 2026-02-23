4b8e761114e2064e5a9ca90e7823367ac8001d29
AyuTUL <ayushtuladhar007@gmail.com>
2026-02-23 18:38:41 +0545

    booo

---
+++ WebTechnology/Lab/4php/dbconnect.php
@@ -0,0 +1,21 @@
+<?php
+$server="localhost";
+$user="root";
+$pass="";
+$conn=new mysqli($server,$user,$pass);
+if($conn->connect_error)
+    {
+        die("Connection Failed ".$conn->connect_error);
+    }
+    echo "connection succesful<br>";
+    $sql="CREATE DATABASE CSIT_A";
+    if(($conn->query($sql))===TRUE)
+        {
+            echo "database created";
+        }
+        else
+            {
+                echo "database creation failed";
+            }
+
+?>
---
+++ WebTechnology/Lab/4php/insert.php
@@ -0,0 +1,23 @@
+<?php
+$server="localhost";
+$user="root";
+$pass="";
+$db="CSIT_A";
+$conn=new mysqli($server,$user,$pass,$db);
+if($conn->connect_error)
+    {
+        die("Connection Failed ".$conn->connect_error);
+    }
+    echo "connection succesful<br>";
+
+    $sql="INSERT INTO employee(eid,name)VALUES(1,'Nirjala'),(2,'Nirjal');";
+    if(($conn->query($sql))===TRUE)
+        {
+            echo "record inserted";
+        }
+        else
+            {
+                echo "insertion failed";
+            }
+
+?>
