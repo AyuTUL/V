aaabddf40c103dee14f97e9d1ad242ad73469be7
AyuTUL <ayushtuladhar007@gmail.com>
2026-02-26 10:20:44 +0545

    fuckjs

--- WebTechnology/Lab/4php/table create.php
+++ WebTechnology/Lab/4php/table create.php
@@ -1,22 +1,19 @@
 <?php
-$server="localhost";
-$user="root";
-$pass="";
-$db="CSIT_A";
-$conn=new mysqli($server,$user,$pass,$db);
-if($conn->connect_error)
-    {
-        die("Connection Failed ".$conn->connect_error);
-    }
-    echo "connection succesful<br>";
-  $sql="CREATE TABLE employee(eid int primary key, name varchar(20));";
-    if(($conn->query($sql))===TRUE)
-        {
-            echo "table created";
-        }
-        else
-            {
-                echo "table creation failed";
-            }
-
+$server = "localhost";
+$user = "root";
+$pass = "";
+$db = "CSIT_A";
+$conn = new mysqli($server, $user, $pass, $db);
+if ($conn->connect_error)
+    die("Connection Failed: " . $conn->connect_error);
+echo "Connection successful<br>";
+$sql = "CREATE TABLE employee (
+    eid INT PRIMARY KEY,
+    name VARCHAR(20)
+)";
+if ($conn->query($sql) === TRUE)
+    echo "Table created successfully";
+else
+    echo "Table creation failed: " . $conn->error;
+$conn->close();
 ?>
