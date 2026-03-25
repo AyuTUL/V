5c9c8267d9f1615085018a97cba5bfd6dbb68d21
AyuTUL <ayushtuladhar007@gmail.com>
2026-03-25 22:06:02 +0545

    jetpackcatanyahu

---
+++ Web Technology/Lab/4php/class/db update.php
@@ -0,0 +1,17 @@
+<?php
+    $conn=new mysqli("localhost","root","","studenthub");
+    if($conn->connect_error)
+            {
+                die("Connection error".$conn->connect_error);
+            }
+            $sql="UPDATE userdetail SET password='Hero' WHERE username='hrikesh'";
+            $result=$conn->query($sql);
+            if(mysqli_affected_rows($conn)>0)
+                {
+                    echo "Update succesful";
+                    echo "No of records updated=".mysqli_affected_rows($conn);
+                }
+                else{
+                    echo "No record updated";
+                }
+        ?>
